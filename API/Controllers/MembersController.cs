f7ce0f879036bd92fd8e8d5f520394e431b82f28
Dantsls22 <danitenasalas2003@gmail.com>
2025-09-07 18:11:14 -0600

    Parcial01 - controladores2

---
+++ API/Controllers/MembersController.cs
@@ -0,0 +1,29 @@
+using API.Data;
+using Microsoft.AspNetCore.Mvc;
+using API.Entities;
+
+namespace API.Controllers;
+
+[Route("api/[controller]")] //localhost:5000/api/members
+[ApiController]
+public class MembersController(AppDbContext context) : ControllerBase
+{
+    [HttpGet] //primer endpoint: localhost:5000/api/members
+    public ActionResult<IReadOnlyList<AppUser>> GetMembers() //metodo para obtener todos los miembros
+    //la lista contiene appuser
+    //con IReadOnlyList se usa menos memoria que con List
+    {
+        var members = context.Users.ToList(); //obtiene todos los usuarios de la base de datos y los convierte en una lista
+        return Ok(members); //devuelve la lista de miembros con un estado 200 OK
+    }
+
+    [HttpGet("{id}")] //https://localhost:5000/api/members/bob-id
+       public ActionResult<AppUser> GetMember(string id) {
+        var member = context.Users.Find(id); //busca un usuario por su id
+        
+        if (member == null) return NotFound(); //si no encuentra el usuario, devuelve un estado 404 Not Found
+        
+        return member; //devuelve el usuario encontrado
+
+    }
+}
