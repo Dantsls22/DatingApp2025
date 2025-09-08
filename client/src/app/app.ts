ab568883d2f51e7db654459c829661dd3b00182e
Dantsls22 <danitenasalas2003@gmail.com>
2025-09-08 00:55:08 -0600

    parcial01_paginaAngular

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,12 +1,28 @@
-import { Component, signal } from '@angular/core';
+//esqueleto basico de una app angular
+import { HttpClient, HttpClientModule} from '@angular/common/http';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, HttpClientModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('client');
+
+export class App implements OnInit {
+
+  private http = inject(HttpClient);
+
+  protected readonly title = signal('Dating App');
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/members').subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error),
+      complete: () => console.log('Completed the http request')
+    });
+  }
 }
