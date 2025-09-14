89c44ed6555bcb1d2a0e76bf12a34a368b62aaa9
Dantsls22 <danitenasalas2003@gmail.com>
2025-09-14 03:23:13 -0600

    parcial01 - Display fetched users

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,28 +1,39 @@
-//esqueleto basico de una app angular
-import { HttpClient, HttpClientModule} from '@angular/common/http';
-import { Component, inject, signal } from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { OnInit } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HttpClientModule],
+  imports: [
+    RouterOutlet,
+    HttpClientModule
+    // habilita HttpClient en standalone
+    // en proyectos grandes, normalmente se recomienda la opción 1 (provideHttpClient()
+    // en main.ts) porque separa responsabilidades
+    // y evita que el root component cargue dependencias globales.
+  ],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']   // ojo: es `styleUrls` en plural
 })
-
 export class App implements OnInit {
 
   private http = inject(HttpClient);
 
-  protected readonly title = signal('Dating App');
+  title = signal('Dating App');
+  members = signal<any>([]);   // puedes tiparlo con una interfaz si lo deseas
 
-  ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/members').subscribe({
-      next: response => console.log(response),
-      error: error => console.log(error),
-      complete: () => console.log('Completed the http request')
-    });
+  async ngOnInit(): Promise<void> {
+    this.members.set(await this.getMembers());
+  }
+
+  async getMembers(): Promise<Object> {
+    try {
+      return lastValueFrom(this.http.get('https://localhost:5001/api/members'))
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
   }
 }
