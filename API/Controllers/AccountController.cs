dbe49fce91d994db205649ba9520604c8f62965d
Dantsls22 <danitenasalas2003@gmail.com>
2025-09-28 20:38:29 -0600

    parcial02 - add token to login/register response

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -4,19 +4,20 @@ using System.Text;
 using API.Data;
 using API.DTOs;
 using API.Entities;
+using API.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
 //empezamos a crear nuestros metodos
-public class AccountController(AppDbContext context) : BaseApiController
+public class AccountController(AppDbContext context, ITokenService tokenService) : BaseApiController
 {
     [HttpPost("register")]
-    public async Task<ActionResult<AppUser>> Register(RegisterRequest request)
+    public async Task<ActionResult<UserResponse>> Register(RegisterRequest request)
     {
-        if(await EmailExists(request.Email)) return BadRequest("Email is already in use");
-       
+        if (await EmailExists(request.Email)) return BadRequest("Email is already in use");
+
         using var hmac = new HMACSHA512(); //son usings distintos
 
         var user = new AppUser
@@ -28,10 +29,18 @@ public class AccountController(AppDbContext context) : BaseApiController
         };
         context.Users.Add(user);
         await context.SaveChangesAsync();
-        return user;
+        
+        return new UserResponse
+        {
+            Id = user.Id,
+            Email = user.Email,
+            DisplayName = user.DisplayName,
+            Token = tokenService.CreateToken(user)
+        };
     }
     [HttpPost("login")]
-    public async Task<ActionResult<AppUser>> Login(LoginRequest request)
+    
+    public async Task<ActionResult<UserResponse>> Login(LoginRequest request)
     {
         var user = await context.Users.SingleOrDefaultAsync(u => u.Email == request.Email);
 
@@ -41,14 +50,20 @@ public class AccountController(AppDbContext context) : BaseApiController
 
         var ComputeHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(request.Password));
 
-        for(var i = 0; i < ComputeHash.Length; i++)
+        for (var i = 0; i < ComputeHash.Length; i++)
         {
             if (ComputeHash[i] != user.PasswordHash[i])
             {
                 return Unauthorized("Invalid email or password");
             }
         }
-        return user;
+        return new UserResponse
+        {
+            Id = user.Id,
+            Email = user.Email,
+            DisplayName = user.DisplayName,
+            Token = tokenService.CreateToken(user)
+        };
     }
     
 
