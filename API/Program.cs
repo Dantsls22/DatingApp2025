72daf2410d11e275a58282065d39e9e00e076f8c
Dantsls22 <danitenasalas2003@gmail.com>
2025-09-28 18:03:44 -0600

    parcial02 - addtokenservice

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,6 @@
 using API.Data;
+using API.Interfaces;
+using API.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,6 +15,7 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 // agregar el contexto de la base de datos
 
 builder.Services.AddCors(); // agregar CORS
+builder.Services.AddScoped<ITokenService, TokenService>(); // agregar el servicio de token como singleton
 
 var app = builder.Build();
 
