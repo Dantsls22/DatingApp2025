22acf891bfe86f3eb8c66737ea809e86165f5ee2
Dantsls22 <danitenasalas2003@gmail.com>
2025-10-19 16:38:48 -0600

    parcial02  - add nav links

--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { inject } from '@angular/core'; // Importamos la funcion inject
 import { AccountService } from '../../core/services/account-service';
-
+import { RouterLink } from '@angular/router';
+import { RouterLinkActive } from '@angular/router';
 @Component({
   selector: 'app-nav',
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink, RouterLinkActive],
   templateUrl: './nav.html',
   styleUrl: './nav.css'
 })
