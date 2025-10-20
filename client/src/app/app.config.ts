35094b76dcf541966b646018b0302d198a8c51ee
Dantsls22 <danitenasalas2003@gmail.com>
2025-10-19 21:23:26 -0600

    parcial02 - add app init

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,14 +1,35 @@
-import { ApplicationConfig, importProvidersFrom, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, inject, provideAppInitializer, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
 import { routes } from './app.routes'; //esto depende de si el archivo existe
+import { InitService } from '../core/services/init-service';
+import { last, lastValueFrom } from 'rxjs';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes),
-    importProvidersFrom(HttpClientModule) //  agregado
+    importProvidersFrom(HttpClientModule), //  agregado
+    provideAppInitializer(async () => {
+      // Aquí puedes agregar lógica de inicialización si es necesario
+
+      const initService = inject(InitService);
+
+      return new Promise<void>((resolve) => {
+        setTimeout(async () => {
+          try {
+            return lastValueFrom(initService.init())
+          } finally {
+            const splash = document.getElementById("initial-splash");
+            if (splash) {
+              splash.remove();
+            }
+            resolve();
+          }
+        }, 500); // Simula un retardo de 500 ms
+      });
+    })
   ]
 };
