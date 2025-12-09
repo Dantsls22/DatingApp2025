df385b1926ee534d2b21b573775673be74388261
Dantsls22 <danitenasalas2003@gmail.com>
2025-12-09 09:56:36 -0600

    parcial03_imageprofile

--- API/Mappers/AppUserMapper.cs
+++ API/Mappers/AppUserMapper.cs
@@ -3,17 +3,18 @@ using API.DTOs;
 using API.Entities;
 using API.Interfaces;
 
-namespace API.Extensions;
+namespace API.Mappers;
 
-public static class AppUserExtensions
+public static class AppUserMapper
 {
     public static UserResponse ToDto(this AppUser user, ITokenService tokenService)
     {
         return new UserResponse
         {
             Id = user.Id,
-            Email = user.Email,
             DisplayName = user.DisplayName,
+            Email = user.Email,
+            ImageUrl = user.ImageUrl,
             Token = tokenService.CreateToken(user)
         };
     }
