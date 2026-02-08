833f09b083f9f26aba5b4c2f76216d327f814017
Dantsls22 <danitenasalas2003@gmail.com>
2026-02-08 17:27:05 -0600

    cleanwork - correcciones

--- src/API/Exceptions/APIException.cs
+++ src/API/Exceptions/APIException.cs
@@ -1,9 +1,8 @@
 namespace API.Exceptions;
 
-public class APIException(int statusCode, string message, string? details = null)
+public class ApiException(int statusCode, string message, string? details)
 {
-    public int statusCode { get; set; } = statusCode;
+    public int StatusCode { get; set; } = statusCode;
     public string Message { get; set; } = message;
-
     public string? Details { get; set; } = details;
 }
--- src/API/Middlewares/ExceptionMiddleware.cs
+++ src/API/Middlewares/ExceptionMiddleware.cs
@@ -22,8 +22,8 @@ public class ExceptionMiddleware(
             context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
 
             var response = env.IsDevelopment() ?
-                new APIException(context.Response.StatusCode, ex.Message, ex.StackTrace) :
-                new APIException(context.Response.StatusCode, ex.Message, "The provided params are incorrect");
+                new ApiException(context.Response.StatusCode, ex.Message, ex.StackTrace) :
+                new ApiException(context.Response.StatusCode, ex.Message, "The provided params are incorrect");
 
             var option = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
             var json = JsonSerializer.Serialize(response, option);
@@ -37,8 +37,8 @@ public class ExceptionMiddleware(
             context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
 
             var response = env.IsDevelopment() ?
-                new APIException(context.Response.StatusCode, ex.Message, ex.StackTrace) :
-                new APIException(context.Response.StatusCode, ex.Message, "Internal server error");
+                new ApiException(context.Response.StatusCode, ex.Message, ex.StackTrace) :
+                new ApiException(context.Response.StatusCode, ex.Message, "Internal server error");
 
             var option = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
             var json = JsonSerializer.Serialize(response, option);
