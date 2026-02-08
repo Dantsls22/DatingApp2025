c2abbbe53caef5dbd5cd54eaf4b832ea12677553
Dantsls22 <danitenasalas2003@gmail.com>
2026-02-08 16:43:11 -0600

    cleanwork - account controller

--- src/API/Controllers/AccountController.cs
+++ src/API/Controllers/AccountController.cs
@@ -11,31 +11,9 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-/// <summary>
-/// Account controller.
-/// </summary>
+//empezamos a crear nuestros metodos
 public class AccountController(AppDbContext context, ITokenService tokenService) : BaseApiController
 {
-    /// <summary>
-    /// Creates an User.
-    /// </summary>
-    /// <param name="request"></param>
-    /// <returns>A newly created account.</returns>
-    /// <remarks>
-    /// Sample request:
-    ///
-    ///     POST api/account/register
-    ///     {
-    ///        "displayName": "displayName",
-    ///        "email": "test@test.com",
-    ///        "password": "password"
-    ///     }
-    ///
-    /// </remarks>
-    /// <response code="200">Returns the newly created item</response>
-    /// <response code="400">If the item is null</response>
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [HttpPost("register")]
     public async Task<ActionResult<UserResponse>> Register(RegisterRequest request)
     {
@@ -55,15 +33,7 @@ public class AccountController(AppDbContext context, ITokenService tokenService)
 
         return user.ToDto(tokenService);
 
-    /// <summary>
-    /// Login as an User.
-    /// </summary>
-    /// <param name="request"></param>
-    /// <returns>Valid user token.</returns>
-    /// <response code="200">Returns the valid user token</response>
-    /// <response code="401">If the username or password are incorrect</response>
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    }
     [HttpPost("login")]
 
     public async Task<ActionResult<UserResponse>> Login(LoginRequest request)
@@ -85,6 +55,7 @@ public class AccountController(AppDbContext context, ITokenService tokenService)
         }
 
         return user.ToDto(tokenService);
+
     }
 
     private async Task<bool> EmailExists(string email)
